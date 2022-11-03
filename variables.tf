6996f14591d68faac59440dd57067821560921f3
Ramkumar Devanathan <ramkumardevanathan@gmail.com>
2022-11-03 12:53:13 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "azure_subscription_id" {}
+
+variable "azure_client_id" {}
+
+variable "azure_client_secret" {}
+
+variable "azure_tenant_id" {}
+
+variable "azure_rgname" {}
+
